e139c91e4ee84b406757f9403c0bbff5db40ed20
Roman Knyazev <roman0981998@yandex.ru>
2019-07-22 22:44:49 +0300

    Activity с кофе #6
    Создана активити с кофе
    Создан общий метод для передачи Bundle в активити
    Общий рефакторинг

--- app/src/main/java/com/example/espcoffee/BeverageActivity.java
+++ app/src/main/java/com/example/espcoffee/BeverageActivity.java
@@ -1,12 +1,40 @@
 package com.example.espcoffee;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class BeverageActivity extends MainActivity {
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beverage_dispensing);
+        Bundle bundle = getIntent().getBundleExtra(Intent.EXTRA_RESTRICTIONS_BUNDLE);
+        if (bundle.containsKey("titleId")) {
+            int titleId = bundle.getInt("titleId");
+            setTitle(titleId);
+        }
+        if (bundle.containsKey("coffeeImageId")) {
+            int coffeeImage = bundle.getInt("coffeeImageId");
+            ImageView coffeeImageView = findViewById(R.id.beverageCoffeeImage);
+            coffeeImageView.setImageResource(coffeeImage);
+        }
+        if (bundle.containsKey("url")) {
+            url = bundle.getString("url");
+        }
+        if (bundle.containsKey("textId")) {
+            int textId = bundle.getInt("textId");
+            TextView textView = findViewById(R.id.beverageCoffeeText);
+            textView.setText(textId);
+        }
+    }
+
+
+    public void startDispensing(View view) {
+        sendGetRequest(url);
     }
 }
